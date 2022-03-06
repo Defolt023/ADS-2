4115ec83f9b99ad3296c37150328b1f09993842d
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-03-06 22:32:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,11 +27,11 @@ double calcItem(double x, uint16_t n) {
 }
 double expn(double x, uint16_t count) {
   for (int n = 1; n <= count; n++) {
-  res += pown(x, n)/fact(n); 
+  res += pown(x, n)/fact(n);
   }
   return res;
 }
-double sinn(double x, uint16_t count) { 
+double sinn(double x, uint16_t count) {
   for (int i = 1; i <= count; i++) {
   res += pown(-1.0, (i - 1)) * calcItem(x, (2 * i - 1));
   }
